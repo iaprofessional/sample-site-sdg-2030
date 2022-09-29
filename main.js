4d1942c00d4abdbdcaa6f7c6bda6d5a70a6a23ca
iaprofessional <75013595+iaprofessional@users.noreply.github.com>
2022-09-29 18:04:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,60 @@
-function show_info() {
-    $('#exampleModal').modal()
+function show_overview() {
+    $('#overviewModal').modal()
+}
+function show_goal() {
+    $('#goalModal').modal()
+}
+function goal_1() {
+   window.open("https://sdgs.un.org/goals/goal1", "_blank")
+}
+function goal_2() {
+   window.open("https://sdgs.un.org/goals/goal2", "_blank")
+}
+function goal_3() {
+   window.open("https://sdgs.un.org/goals/goal3", "_blank")
+}
+function goal_4() {
+   window.open("https://sdgs.un.org/goals/goal4", "_blank")
+}
+function goal_5() {
+   window.open("https://sdgs.un.org/goals/goal5", "_blank")
+}
+function goal_6() {
+   window.open("https://sdgs.un.org/goals/goal6", "_blank")
+}
+function goal_7() {
+   window.open("https://sdgs.un.org/goals/goal7", "_blank")
+}
+function goal_8() {
+   window.open("https://sdgs.un.org/goals/goal8", "_blank")
+}
+function goal_9() {
+   window.open("https://sdgs.un.org/goals/goal9", "_blank")
+}
+function goal_10() {
+   window.open("https://sdgs.un.org/goals/goal10", "_blank")
+}
+function goal_11() {
+   window.open("https://sdgs.un.org/goals/goal11", "_blank")
+}
+function goal_12() {
+   window.open("https://sdgs.un.org/goals/goal12", "_blank")
+}
+function goal_13() {
+   window.open("https://sdgs.un.org/goals/goal13", "_blank")
+}
+function goal_14() {
+   window.open("https://sdgs.un.org/goals/goal14", "_blank")
+}
+function goal_15() {
+   window.open("https://sdgs.un.org/goals/goal15", "_blank")
+}
+function goal_16() {
+   window.open("https://sdgs.un.org/goals/goal16", "_blank")
+}
+function goal_17() {
+   window.open("https://sdgs.un.org/goals/goal17", "_blank")
+}
+function more() {
+   window.open("https://sdgs.un.org/goals", "_blank")
 }
